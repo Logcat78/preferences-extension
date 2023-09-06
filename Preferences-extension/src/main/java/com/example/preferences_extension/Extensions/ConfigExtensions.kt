730a4768d7d548a2b587ee606057b88d4fc88b15
ITSTAS2009RUS <marioblamp@yandex.ru>
2023-09-06 23:31:08 +0300

    Add new Extensions

--- Preferences-extension/src/main/java/com/example/preferences_extension/Extensions/ConfigExtensions.kt
+++ Preferences-extension/src/main/java/com/example/preferences_extension/Extensions/ConfigExtensions.kt
@@ -17,18 +17,158 @@ fun String.saveDataWithConfig(config: List<Any>, key: String){
             is String -> table = it
             is Int -> mode = it
             is Boolean -> isAsync = it
-
-
-
-            else -> {
-                // Тут надо релизовать ошибку
-            }
+            else -> throw TypeCastException("The function does not accept the given type")
         }
     }
     val sharedPreferences: SharedPreferences =
         context!!.getSharedPreferences(table, mode!!)
     val editor = sharedPreferences.edit()
     editor.putString(key, this)
+
+    if(isAsync!!){
+        editor.apply()
+    }else{
+        editor.commit()
+    }
+
+}
+
+fun Int.saveDataWithConfig(config: List<Any>, key: String){
+    var context: Context? = null
+    var table: String? = null
+    var mode: Int? = null
+    var isAsync:Boolean? = null
+    Log.d("gp", "Данные сохранены")
+
+    config.forEach {
+        when (it) {
+            is Context -> context = it
+            is String -> table = it
+            is Int -> mode = it
+            is Boolean -> isAsync = it
+            else -> throw TypeCastException("The function does not accept the given type")
+        }
+    }
+    val sharedPreferences: SharedPreferences =
+        context!!.getSharedPreferences(table, mode!!)
+    val editor = sharedPreferences.edit()
+    editor.putInt(key, this)
+
+    if(isAsync!!){
+        editor.apply()
+    }else{
+        editor.commit()
+    }
+
+}
+
+fun Float.saveDataWithConfig(config: List<Any>, key: String){
+    var context: Context? = null
+    var table: String? = null
+    var mode: Int? = null
+    var isAsync:Boolean? = null
+    Log.d("gp", "Данные сохранены")
+
+    config.forEach {
+        when (it) {
+            is Context -> context = it
+            is String -> table = it
+            is Int -> mode = it
+            is Boolean -> isAsync = it
+            else -> throw TypeCastException("The function does not accept the given type")
+        }
+    }
+    val sharedPreferences: SharedPreferences =
+        context!!.getSharedPreferences(table, mode!!)
+    val editor = sharedPreferences.edit()
+    editor.putFloat(key, this)
+
+    if(isAsync!!){
+        editor.apply()
+    }else{
+        editor.commit()
+    }
+
+}
+
+fun Boolean.saveDataWithConfig(config: List<Any>, key: String){
+    var context: Context? = null
+    var table: String? = null
+    var mode: Int? = null
+    var isAsync:Boolean? = null
+    Log.d("gp", "Данные сохранены")
+
+    config.forEach {
+        when (it) {
+            is Context -> context = it
+            is String -> table = it
+            is Int -> mode = it
+            is Boolean -> isAsync = it
+            else -> throw TypeCastException("The function does not accept the given type")
+        }
+    }
+    val sharedPreferences: SharedPreferences =
+        context!!.getSharedPreferences(table, mode!!)
+    val editor = sharedPreferences.edit()
+    editor.putBoolean(key, this)
+
+    if(isAsync!!){
+        editor.apply()
+    }else{
+        editor.commit()
+    }
+
+}
+
+fun Long.saveDataWithConfig(config: List<Any>, key: String){
+    var context: Context? = null
+    var table: String? = null
+    var mode: Int? = null
+    var isAsync:Boolean? = null
+    Log.d("gp", "Данные сохранены")
+
+    config.forEach {
+        when (it) {
+            is Context -> context = it
+            is String -> table = it
+            is Int -> mode = it
+            is Boolean -> isAsync = it
+            else -> throw TypeCastException("The function does not accept the given type")
+        }
+    }
+    val sharedPreferences: SharedPreferences =
+        context!!.getSharedPreferences(table, mode!!)
+    val editor = sharedPreferences.edit()
+    editor.putLong(key, this)
+
+    if(isAsync!!){
+        editor.apply()
+    }else{
+        editor.commit()
+    }
+
+}
+
+fun Set<String>.saveDataWithConfig(config: List<Any>, key: String){
+    var context: Context? = null
+    var table: String? = null
+    var mode: Int? = null
+    var isAsync:Boolean? = null
+    Log.d("gp", "Данные сохранены")
+
+    config.forEach {
+        when (it) {
+            is Context -> context = it
+            is String -> table = it
+            is Int -> mode = it
+            is Boolean -> isAsync = it
+            else -> throw TypeCastException("The function does not accept the given type")
+        }
+    }
+    val sharedPreferences: SharedPreferences =
+        context!!.getSharedPreferences(table, mode!!)
+    val editor = sharedPreferences.edit()
+    editor.putStringSet(key, this)
 
     if(isAsync!!){
         editor.apply()
